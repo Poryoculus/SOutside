ac874a48e9b64b234143c75a15e51dc19d69c4a8
Poryoculus <rodrigopachecojor@gmail.com>
2025-10-04 13:53:28 -0300

    saving files

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,6 +3,7 @@ import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
+
 const category = getParam("category");
 const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
